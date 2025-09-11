5cda14a4d605f43a81a1ea681a8ffc80c46875e4
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-11 14:59:58 +0530

    backend for signup + login is ready.

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+mongoose.connect('mongodb+srv://venomc381:Venom%4054321@cluster0.yfrink8.mongodb.net/');
+
+const kahUsers = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+});
+
+const users = mongoose.model('users', kahUsers);
+
+export default users;
