5cc502973bd36db8159c0265bb9d4b90f00d0ee8
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-12 13:00:16 +0530

    AI Tours title change.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -39,7 +39,7 @@ function CustomerInfo() {
 function Tours() {
   return (
     <Flex  flexDirection={"column"} mt={{base: "0vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}} >
-      <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}} >Tours</Text>
+      <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}} >AI Tours</Text>
       <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
         <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "0vw"}}>
           <Image src="./Gradient4.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
