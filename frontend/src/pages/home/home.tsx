34f31ec96a10b465fe18b9b22a76c7b4cc3cf57b
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-12 12:23:01 +0530

    home-page checkpoint-v1.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -10,6 +10,7 @@ function Home() {
       <Tours />
       <Discover />
   
+  
     </>
     
   )
@@ -17,7 +18,7 @@ function Home() {
 
 function CustomerInfo() {
   return(
-    <Flex justifyContent={"center"} gap={{base: "10vw"}} mt={{base: "5vw"}}>
+    <Flex justifyContent={"center"} gap={{base: "9vw"}} mt={{base: "7vw"}}>
       <Flex direction={"column"} textAlign={"center"} width={{base: "25vw"}}>
         <Text textStyle={"5xl"} fontWeight={"600"}>9</Text>
         <Text textStyle={"md"} fontWeight={"300"}>Friends</Text>
@@ -41,7 +42,7 @@ function Tours() {
     <Flex  flexDirection={"column"} mt={{base: "0vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}} >
       <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}} >Tours</Text>
       <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
-        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "0.5vw"}}>
+        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "0vw"}}>
           <Image src="./Gradient4.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
           <Text position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" color="#fff" textStyle="2xl">City</Text>
         </Box>
@@ -68,7 +69,7 @@ function Discover() {
   return (
     <Flex  flexDirection={"column"} mt={{base: "-4vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
       <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}}>Discover</Text>
-      <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
+      <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" ml={{base: "-2vw"}} css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
         <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "2.5vw"}}>
           <Image src="./Gradient5.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
           <Text position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" color="#fff" textStyle="2xl">Shops</Text>
@@ -85,6 +86,7 @@ function Discover() {
     </Flex>
   )
 }
+
 
 
 
