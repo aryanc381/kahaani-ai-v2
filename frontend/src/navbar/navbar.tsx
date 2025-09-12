635c08470ec4f5a3a5ce0e093971f2be3e057374
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-12 09:58:29 +0530

    homepage started.

--- frontend/src/navbar/navbar.tsx
+++ frontend/src/navbar/navbar.tsx
@@ -1,29 +1,19 @@
-import { Button, Flex, Menu, Portal } from "@chakra-ui/react"
+import { Avatar, Button, Flex, HStack, Menu, Portal, Text } from "@chakra-ui/react"
+
 
 function Navbar() {
   return (
-    <Flex justifyContent={{base: "space-between", md: "center"}} mt={{base: "3vw"}} ml={{base: "3vw"}} mr={{base: "3vw"}} backgroundColor={{base:"#55555544"}} p={{base:"3vw"}} borderRadius={{base:"1vw", md:"0.5vw"}} borderWidth={{base: "0.5vw"}} borderColor={"#2b2b2bff"}>
-        <Flex mt={{base:"1vw", md: "0.5vw"}}>
-            <p>KahaaniAI</p>
-        </Flex>
-        <div>
-            <Menu.Root>
-                <Menu.Trigger asChild>
-                    <Button variant={"surface"} size="sm" fontWeight={{base: "bolder"}}>
-                    Menu
-                    </Button>
-                </Menu.Trigger>
-                <Portal>
-                    <Menu.Positioner>
-                    <Menu.Content>
-                        <Menu.Item value="Settings">Settings</Menu.Item>
-                        <Menu.Item value="Profile">Payment</Menu.Item>
-                        <Menu.Item value="Logout" color="fg.error" _hover={{ bg: "bg.error", color: "fg.error" }}>Logout</Menu.Item>
-                    </Menu.Content>
-                    </Menu.Positioner>
-                </Portal>
-            </Menu.Root>
-        </div>
+    <Flex flexDirection={"column"} mt={{base: "2vw"}} mr={{base: "5vw"}} ml={{base: "5vw"}}>
+      <Flex justifyContent={"space-between"}>
+        <Text textStyle={{base: "3xl"}} mt={{base: "1.5vw", md: "0.5vw"}} letterSpacing={{base: "-0.3vw"}}>Hello, Aryan.</Text>
+        <HStack gap="4">
+      <Avatar.Root colorPalette="green">
+        <Avatar.Fallback />
+        <Avatar.Image src="https://bit.ly/broken-link" />
+      </Avatar.Root>
+      </HStack>
+      </Flex>
+      
     </Flex>
   )
 }
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,8 +1,61 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { ButtonGroup, IconButton, Pagination } from "@chakra-ui/react"
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi"
+import { Avatar, HStack, defineStyle } from "@chakra-ui/react"
+import { Grid } from "@chakra-ui/react"
+import Navbar from "../../navbar/navbar";
+
+const ringCss = defineStyle({
+  outlineWidth: "0.3vw",
+  outlineColor: "colorPalette.500",
+  outlineOffset: "0.3vw",
+  outlineStyle: "dotted",
+})
+
 function Home() {
+  return(
+    <>
+      <Navbar />
+      <Tours />
+      <Discover />
+    </>
+    
+  )
+}
+
+function Tours() {
   return (
-    <div className="flex jusitfy-between">
-      <div className="">KahaaniAI</div>
-    </div>
+    <Flex  flexDirection={"column"} mt={{base: "0vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
+      <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}}>Tours</Text>
+      <Flex>
+        <Box position={{base: "relative"}} display={{base: "inline-block"}} ml={{base: "0.5vw"}}>
+          <Image src="./Gradient4.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+          <Text position={"absolute"} bottom={"16vw"} left={"15vw"} color={"#fff"} textStyle={{base: "2xl"}}>City</Text>
+        </Box>
+        <Box position={{base: "relative"}} display={{base: "inline-block"}} ml={{base: "3vw"}}>
+            <Image src="./Gradient3.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+            <Text position={"absolute"} bottom={"16vw"} left={"5vw"} color={"#fff"} textStyle={{base: "2xl"}}>Monuments</Text>
+        </Box>
+      </Flex>
+    </Flex>
+  )
+}
+
+function Discover() {
+  return (
+    <Flex  flexDirection={"column"} mt={{base: "-4vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
+      <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}}>Discover</Text>
+      <Flex>
+        <Box position={{base: "relative"}} display={{base: "inline-block"}} ml={{base: "0.5vw"}}>
+          <Image src="./Gradient5.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+          <Text position={"absolute"} bottom={"16vw"} left={"12.3vw"} color={"#fff"} textStyle={{base: "2xl"}}>Shops</Text>
+        </Box>
+        <Box position={{base: "relative"}} display={{base: "inline-block"}} ml={{base: "3vw"}}>
+            <Image src="./Gradient6.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+            <Text position={"absolute"} bottom={"16vw"} left={"13.3vw"} color={"#fff"} textStyle={{base: "2xl"}}>Food</Text>
+        </Box>
+      </Flex>
+    </Flex>
   )
 }
 
