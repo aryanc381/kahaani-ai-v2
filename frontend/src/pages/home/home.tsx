98974be633a8645ba67df428b12b615f2ad9f572
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-16 13:52:14 +0530

    added most popular.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -77,18 +77,18 @@ function Discover() {
   return (
     <Flex  flexDirection={"column"} mt={{base: "-4vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
       <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}}>Most Popular</Text>
-      <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" ml={{base: "-2vw"}} css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} mt={"1vw"}>
-        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "2.5vw"}} backgroundColor={"gray.900"} pt={"1vw"} pl={"2vw"} pr={"2vw"} pb={"2vw"} borderRadius={"5vw"}>
-          <Image src="./wada.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
-          <Text position="relative" color="#fff" mt={"-1vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Shaniwar Wada</Text>
+      <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" ml={{base: "-2vw"}} css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
+        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "2.5vw"}}>
+          <Image src="./wada.png" mt={"2.4vw"} width={{base: "40vw"}} borderRadius={{base: "10%"}} />
+          <Text position="relative" color="#fff" mt={"-2vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Shaniwar Wada</Text>
         </Box>
-        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "3vw"}}>
-            <Image src="./Gradient (3).svg" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
-            <Text position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" color="#fff" textStyle="2xl">Food</Text>
+        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "3.5vw"}}>
+            <Image src="./taj-mahal.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+            <Text position="relative" color="#fff" mt={"0.75vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Taj Mahal</Text>
         </Box>
-        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "3vw"}}>
-            <Image src="./sports.png" width={{base: "40vw"}}/>
-            <Text position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" color="#fff" textStyle="2xl">Sports</Text>
+        <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "4.5vw"}}>
+            <Image src="./red-fort.png" mt={"-1.5vw"} width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+            <Text position="relative" color="#fff" mt={"2.5vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Red Fort</Text>
         </Box>
       </Flex>
     </Flex>
