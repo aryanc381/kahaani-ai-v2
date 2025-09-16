2598b5a6ca164731877e30ab54440baa2743f45b
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-16 13:57:20 +0530

    most popular fixed.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -75,7 +75,7 @@ function Tours() {
 
 function Discover() {
   return (
-    <Flex  flexDirection={"column"} mt={{base: "-4vw"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
+    <Flex  flexDirection={"column"} mt={{base: "-4w"}} mr={{base: "5.05vw"}} ml={{base: "5.05vw"}} fontWeight={{base: "500"}} letterSpacing={{base: "-0.2vw"}}>
       <Text textStyle={{base: "4xl"}} mt={{base: "9vw"}} letterSpacing={{base: "-0.3vw"}} mb={{base: "1vw"}}>Most Popular</Text>
       <Flex overflowX="auto" flexWrap="nowrap" gap="0.5vw" ml={{base: "-2vw"}} css={{ "::-webkit-scrollbar": { display: "none" }, scrollbarWidth:"none", "-webkit-overflow-scrolling": "touch"}} >
         <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "2.5vw"}}>
@@ -83,12 +83,12 @@ function Discover() {
           <Text position="relative" color="#fff" mt={"-2vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Shaniwar Wada</Text>
         </Box>
         <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "3.5vw"}}>
-            <Image src="./taj-mahal.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
-            <Text position="relative" color="#fff" mt={"0.75vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Taj Mahal</Text>
+            <Image mt={"0.1vw"} src="./taj-mahal.png" width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
+            <Text position="relative" color="#fff" mt={"0.5vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Taj Mahal</Text>
         </Box>
         <Box position={{base: "relative"}} flex={{base: "0 0 auto"}} ml={{base: "4.5vw"}}>
             <Image src="./red-fort.png" mt={"-1.5vw"} width={{base: "40vw"}} borderRadius={{base: "10%"}}/>
-            <Text position="relative" color="#fff" mt={"2.5vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Red Fort</Text>
+            <Text position="relative" color="#fff" mt={"1.5vw"} textAlign={"center"} textStyle="xl" ml={"1vw"}>Red Fort</Text>
         </Box>
       </Flex>
     </Flex>
