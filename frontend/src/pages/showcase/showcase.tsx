7a3ad27c403d9d6153caf5689b09b55c18c3e5c1
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-09-17 12:46:28 +0530

    Navbar updated.

--- frontend/src/pages/showcase/showcase.tsx
+++ frontend/src/pages/showcase/showcase.tsx
@@ -1,22 +1,20 @@
 import { Globe } from '@/components/ui/globe';
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { ProgressiveBlur } from "../../components/ui/progressive-blur";
-import { useColorMode } from '@/components/ui/color-mode';
-import { VscVr } from 'react-icons/vsc';
+import { useColorMode, useColorModeValue } from '@/components/ui/color-mode';
+import { VscTelescope, VscVr } from 'react-icons/vsc';
 import { useNavigate } from 'react-router-dom';
+
  
 function Showcase() {
   return (
-    <Flex direction={"column"}>
-        <Flex position={"relative"} mt={"20vw"}>
-          <Globe />
-        </Flex>
-        
-     
-        <Navbar />
-        <ProgressiveBlur height="10%" position="bottom" />
-        
-       
+    <Flex>
+      <Flex direction={"column"}>
+          <Navbar />
+          <ProgressiveBlur height="15%" position="bottom" />
+
+          <Header />
+      </Flex>
     </Flex>
   )
 }
@@ -25,12 +23,12 @@ function Showcase() {
 
 function Navbar() {
   const { toggleColorMode } = useColorMode();
+  const overlayColor = useColorModeValue("rgba(255, 255, 255, 1)", "rgba(0, 0, 0, 1)");
   const navigate = useNavigate();
   return (
     <Flex>
-      <ProgressiveBlur height="25%" position="top" />
 
-      <Flex width="full" position="fixed" zIndex={100} top={0} px={{ base: "5vw", md: "2vw" }} py={{ base: "4vw", md: "1.5vw" }}  justifyContent="space-between" alignItems="center">
+      <Flex width="full" backgroundColor={overlayColor} position="fixed" top={0} px={{ base: "5vw", md: "2vw" }} py={{ base: "4vw", md: "1.5vw" }}  justifyContent="space-between" alignItems="center">
         <Flex>
           <Text letterSpacing={{ base: "-0.2vw" }} fontSize={{ base: "6vw", md: "2vw" }}>KahaaniAI</Text>
           <Box ml={{base: "2vw"}} mt={{base: "0.5vw"}}>
@@ -47,6 +45,19 @@ function Navbar() {
       </Flex>
     </Flex>
   );
+}
+
+function Header() {
+  
+  return(
+    <Flex>
+    <Flex alignItems={"center"} direction={"column"} mt={"40vw"} justifyContent={"center"} mr={"2vw"} ml={"2vw"}>
+      <Text fontSize={"7vw"} letterSpacing={"-0.4vw"} textAlign={{base: "center", md: ""}}>Your Personal Guide to Authentic Indian Monuments.</Text>
+      <Button variant="subtle" mt={"2vw"} borderRadius="3vw" px="6vw" py="4">Talk with Vaishvi <VscTelescope /></Button>
+    </Flex>
+    
+    </Flex>
+  )
 }
 
 
